15b0a7cdf20be36d99b6c518071413ab4ca47ae4
Yifan Hao <809073699@qq.com>
2023-01-10 12:29:50 +0200

    Initial commit

---
+++ src/main/java/com/example/birdnest_back/BirdnestBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.birdnest_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdnestBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdnestBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/birdnest_back/BirdnestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.birdnest_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdnestBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
